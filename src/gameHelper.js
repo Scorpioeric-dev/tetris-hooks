1785647ce5963dc02f4456be73b5ea90850e7c22
Scorpioeric-dev <ericscorpio1@gmail.com>
2020-04-21 19:50:14 -0600

    tetris

--- src/gameHelper.js
+++ src/gameHelper.js
@@ -1,8 +1,8 @@
-export const Stage_Width = 17;
-export const Stage_Height = 25;
+export const Stage_Width = 12;
+export const Stage_Height = 20;
 
 export const createStage = () =>
-  Array.from(Array(Stage_Width), () =>
+  Array.from(Array(Stage_Height), () =>
     new Array(Stage_Width).fill([0, "clear"])
   );
 
